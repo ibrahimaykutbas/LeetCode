76886e7776001495b5fb3df5b45ee5603a4bc061
ibrahimaykutbas <ibrahimaykutbas@gmail.com>
2022-08-18 16:00:00 +0300

    Update BestTimeToBuyAndSellStock

--- BestTimeToBuyAndSellStock.js
+++ BestTimeToBuyAndSellStock.js
@@ -29,38 +29,26 @@ let profit = 0;
 let message = "";
 
 const maxProfit = (prices) => {
-    // Hisse fiyatlarını tek tek dolaşıyoruz.
-    prices.map((price, index) => {
-        /*     
-        Eğer array'in ilk elemanı ise 
-        veya son satın alınan fiyat o anki değerden büyük veya eşitse
-        hisse senedini alıyoruz
-        */
-        if (index == 0 || buyPrice >= price) {
-            buyPrice = price;
-            buyDay = index + 1;
-        }
-        /*     
-        Eğer o günki gün son satın alma gününden büyükse
-        veya o günki hisse değeri en yüksekse
-        hisse senedini satıyoruz
-        */
-        if (index + 1 > buyDay && sellPrice <= price) {
-            sellPrice = price;
-            sellDay = index + 1;
-        }
-    });
-    // Güncel karı hesaplıyoruz
-    profit = sellPrice - buyPrice;
+  prices.map((price, index) => {
+    if (index == 0 || buyPrice >= price) {
+      buyPrice = price;
+      buyDay = index + 1;
+    }
 
-    // Eğer kar 0'dan büyükse bilgi veriyoruz
-    // Değilse karın olmadığını belirtiyoruz
-    message =
-        profit > 0
-            ? `Buy on day ${buyDay} (price = ${buyPrice}) and sell on day ${sellDay} (price = ${sellPrice}), profit = ${sellPrice} - ${buyPrice} = ${profit}.`
-            : "No Profit!";
+    if (index + 1 > buyDay && sellPrice <= price) {
+      sellPrice = price;
+      sellDay = index + 1;
+    }
+  });
 
-    console.log(message);
+  profit = sellPrice - buyPrice;
+
+  message =
+    profit > 0
+      ? `Buy on day ${buyDay} (price = ${buyPrice}) and sell on day ${sellDay} (price = ${sellPrice}), profit = ${sellPrice} - ${buyPrice} = ${profit}.`
+      : "No Profit!";
+
+  console.log(message);
 };
 
 maxProfit(prices);
