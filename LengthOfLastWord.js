e7f6543368cb4c77e83af04eacafcf570341b4ca
ibrahimaykutbas <ibrahimaykutbas@gmail.com>
2022-08-18 16:00:12 +0300

    Update LengthOfLastWord

--- LengthOfLastWord.js
+++ LengthOfLastWord.js
@@ -22,21 +22,15 @@ Explanation: The last word is "joyboy" with length 6.
 const str = "Hello There ";
 
 const lengthOfLastWord = (str) => {
-    // İfadenin başındaki ve sonundaki boşlukları kaldırıyoruz
-    // Böylece son eleman eğer boşluksa 0 değeri döndürmesini engellemiş oluyoruz
-    const trimStr = str.trim();
+  const trimStr = str.trim();
 
-    // İfade boşluklara göre bölüyoruz
-    // Bu bize elemanları kelimeler olan bir array döndürüyor
-    const splitStr = trimStr.split(" ");
+  const splitStr = trimStr.split(" ");
 
-    // Son elemanı alıyoruz
-    const lastWord = splitStr.pop();
+  const lastWord = splitStr.pop();
 
-    // Son elemanın değerini alıyoruz
-    const lastWordLen = lastWord.length;
+  const lastWordLen = lastWord.length;
 
-    console.log(lastWordLen);
+  console.log(lastWordLen);
 };
 
 lengthOfLastWord(str);
